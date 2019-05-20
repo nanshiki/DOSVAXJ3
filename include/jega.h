11499af17c683129c76c66d8c3319fa7ede64103
Kobayashi Takashi <support@nanshiki.co.jp>
2019-05-20 16:51:52 +0900

    DOSVAX修正取込

--- include/jega.h
+++ include/jega.h
@@ -18,29 +18,34 @@
 #define BIOSMEM_AX_KBDSTATUS 0xEB
 
 #define BIOS_KEYBOARD_AX_KBDSTATUS 0x4EB
+/* 40h:EBh Keyboard Additional Status bits
+	bit 2 : Kana holding
+	bit 1 : Kana Lock key
+	bit 0 : Kana LED indicating
+*/
 
 /* JEGA internal registers */
 typedef struct {
-	Bitu RMOD1
-		, RMOD2
-		, RDAGS
-		, RDFFB
-		, RDFSB
-		, RDFAP
-		, RPESL
-		, RPULP
-		, RPSSC
-		, RPSSU
-		, RPSSL
-		, RPPAJ
-		, RCMOD
-		, RCCLH
-		, RCCLL
-		, RCCSL
-		, RCCEL
-		, RCSKW
-		, ROMSL
-		, RSTAT
+	Bitu RMOD1//b9: Mode register 1
+		, RMOD2//ba: Mode register 2
+		, RDAGS//bb: ANK Group sel (not implemented)
+		, RDFFB//bc: Font access first byte
+		, RDFSB//bd: Font access second byte
+		, RDFAP//be: Font Access Pattern
+		, RPESL//09: end scan line (superceded by EGA)
+		, RPULP//14: under scan line (superceded by EGA)
+		, RPSSC//db: DBCS start scan line
+		, RPSSU//d9: 2x DBCS upper start scan
+		, RPSSL//da: 2x DBCS lower start scan
+		, RPPAJ//dc: super imposed (only AX-2 system, not implemented)
+		, RCMOD//dd: Cursor Mode (not implemented)
+		, RCCLH//0e: Cursor location Upper bits (superceded by EGA)
+		, RCCLL//0f: Cursor location Lower bits (superceded by EGA)
+		, RCCSL//0a: Cursor Start Line (superceded by EGA)
+		, RCCEL//0b: Cursor End Line (superceded by EGA)
+		, RCSKW//de: Cursor Skew control (not implemented)
+		, ROMSL//df: Unused?
+		, RSTAT//bf: Font register accessible status
 		;
 	Bitu fontIndex = 0;
 } JEGA_DATA;
@@ -48,8 +53,9 @@ typedef struct {
 extern JEGA_DATA jega;
 
 //jfontload.cpp
-extern Bit8u jfont_sbcs_19[256 * 19];//SBCS font 256 * 19( * 8)
-extern Bit8u jfont_dbcs_16[65536 * 32];//DBCS font 65536 * 16 * 2 (* 8)
+extern Bit8u jfont_sbcs_19[];
+extern Bit8u jfont_dbcs_16[];
+extern Bit8u jfont_cache_dbcs_16[];
 // for J-3100
 extern Bit8u jfont_sbcs_16[];
 extern Bit8u jfont_dbcs_24[];
