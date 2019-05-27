292125f496f1eba1363a019cfc6bfd3a58792f85
Kobayashi Takashi <support@nanshiki.co.jp>
2019-05-27 18:02:35 +0900

    Wayland 対策

--- src/gui/sdl_mapper.cpp
+++ src/gui/sdl_mapper.cpp
@@ -2505,6 +2505,10 @@ void MAPPER_Init(void) {
 	}
 }
 //Somehow including them at the top conflicts with something in setup.h
+#if defined(LINUX)
+extern bool debug_flag;
+#endif
+
 #ifdef C_X11_XKB
 #include "SDL_syswm.h"
 #include <X11/XKBlib.h>
@@ -2554,8 +2558,14 @@ void MAPPER_StartUp(Section * sec) {
 					const char* keycodes = XGetAtomName(info.info.x11.display, desc->names->keycodes);
 //					const char* geom = XGetAtomName(info.info.x11.display, desc->names->geometry);
 					if(keycodes) {
+#if defined(LINUX)
+						if(debug_flag) {
+							printf("keyboard type %s",keycodes);
+						}
+#endif
 						LOG(LOG_MISC,LOG_NORMAL)("keyboard type %s",keycodes);
 						if (strncmp(keycodes,"evdev",5) == 0) evdev_input = true;
+						if (strncmp(keycodes,"(unnamed)",9) == 0) evdev_input = true;
 					}
 					XkbFreeNames(desc,XkbAllNamesMask,True);
 				}
