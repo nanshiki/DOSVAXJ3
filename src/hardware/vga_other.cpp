c1d810b43988eabc38a41f39aa63c66d69db4f64
Kobayashi Takashi <support@nanshiki.co.jp>
2022-03-30 17:26:17 +0900

    DOSBox SVN

--- src/hardware/vga_other.cpp
+++ src/hardware/vga_other.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2002-2015  The DOSBox Team
+ *  Copyright (C) 2002-2021  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -11,10 +11,11 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+
 
 #include <string.h>
 #include <math.h>
@@ -106,7 +107,7 @@ static void write_crtc_data_other(Bitu /*port*/,Bitu val,Bitu /*iolen*/) {
 		vga.other.lightpen |= (Bit8u)val;
 		break;
 	default:
-		LOG(LOG_VGAMISC,LOG_NORMAL)("MC6845:Write %X to illegal index %x",val,vga.other.index);
+		LOG(LOG_VGAMISC,LOG_NORMAL)("MC6845:Write %" sBitfs(X) " to illegal index %x",val,vga.other.index);
 	}
 }
 static Bitu read_crtc_data_other(Bitu /*port*/,Bitu /*iolen*/) {
@@ -153,7 +154,7 @@ static Bitu read_crtc_data_other(Bitu /*port*/,Bitu /*iolen*/) {
 	return (Bitu)(~0);
 }
 
-static void write_lightpen(Bitu port,Bitu val,Bitu) {
+static void write_lightpen(Bitu port,Bitu /*val*/,Bitu) {
 	switch (port) {
 	case 0x3db:	// Clear lightpen latch
 		vga.other.lightpen_triggered = false;
@@ -161,11 +162,11 @@ static void write_lightpen(Bitu port,Bitu val,Bitu) {
 	case 0x3dc:	// Preset lightpen latch
 		if (!vga.other.lightpen_triggered) {
 			vga.other.lightpen_triggered = true; // TODO: this shows at port 3ba/3da bit 1
-			
+
 			double timeInFrame = PIC_FullIndex()-vga.draw.delay.framestart;
 			double timeInLine = fmod(timeInFrame,vga.draw.delay.htotal);
 			Bitu current_scanline = (Bitu)(timeInFrame / vga.draw.delay.htotal);
-			
+
 			vga.other.lightpen = (Bit16u)((vga.draw.address_add/2) * (current_scanline/2));
 			vga.other.lightpen += (Bit16u)((timeInLine / vga.draw.delay.hdend) *
 				((float)(vga.draw.address_add/2)));
@@ -279,9 +280,9 @@ static void update_cga16_color(void) {
 	}
 	Bitu CGApal[4] = {
 		overscan,
-		2 + (color_sel||bw ? 1 : 0) + (background_i ? 8 : 0),
-		4 + (color_sel&&!bw? 1 : 0) + (background_i ? 8 : 0),
-		6 + (color_sel||bw ? 1 : 0) + (background_i ? 8 : 0)
+		static_cast<Bitu>(2 + (color_sel||bw ? 1 : 0) + (background_i ? 8 : 0)),
+		static_cast<Bitu>(4 + (color_sel&&!bw? 1 : 0) + (background_i ? 8 : 0)),
+		static_cast<Bitu>(6 + (color_sel||bw ? 1 : 0) + (background_i ? 8 : 0))
 	};
 	for (Bit8u x=0; x<4; x++) {	 // Position of pixel in question
 		bool even = (x & 1) == 0;
@@ -380,6 +381,8 @@ static void write_cga_color_select(Bitu val) {
 		vga.tandy.border_color = val & 0xf;
 		vga.attr.overscan_color = 0;
 		break;
+	default: //Else unhandled values warning
+		break;
 	}
 }
 
@@ -390,7 +393,7 @@ static void write_cga(Bitu port,Bitu val,Bitu /*iolen*/) {
 	switch (port) {
 	case 0x3d8:
 		vga.tandy.mode_control=(Bit8u)val;
-		vga.attr.disabled = (val&0x8)? 0: 1; 
+		vga.attr.disabled = (val&0x8)? 0: 1;
 		if (vga.tandy.mode_control & 0x2) {		// graphics mode
 			if (vga.tandy.mode_control & 0x10) {// highres mode
 				if (cga_comp==1 || (cga_comp==0 && !(val&0x4))) {	// composite display
@@ -424,7 +427,7 @@ static void CGAModel(bool pressed) {
 	update_cga16_color();
 	LOG_MSG("%s model CGA selected", new_cga ? "Late" : "Early");
 }
- 
+
 static void Composite(bool pressed) {
 	if (!pressed) return;
 	if (++cga_comp>2) cga_comp=0;
@@ -454,7 +457,7 @@ static void tandy_update_palette() {
 				if (vga.tandy.color_select & 0x10) color_set |= 8; // intensity
 				if (vga.tandy.color_select & 0x20) color_set |= 1; // Cyan Mag. White
 				if (vga.tandy.mode_control & 0x04) {			// Cyan Red White
-					color_set |= 1; 
+					color_set |= 1;
 					r_mask &= ~1;
 				}
 				VGA_SetCGA4Table(
@@ -647,20 +650,20 @@ static void write_pcjr(Bitu port,Bitu val,Bitu /*iolen*/) {
 		break;
 	case 0x3df:
 		// CRT/processor page register
-		
+
 		// Bit 0-2: CRT page PG0-2
 		// In one- and two bank modes, bit 0-2 select the 16kB memory
 		// area of system RAM that is displayed on the screen.
 		// In 4-banked modes, bit 1-2 select the 32kB memory area.
 		// Bit 2 only has effect when the PCJR upgrade to 128k is installed.
-		
+
 		// Bit 3-5: Processor page CPU_PG
 		// Selects the 16kB area of system RAM that is mapped to
-		// the B8000h IBM PC video memory window. Since A14-A16 of the 
+		// the B8000h IBM PC video memory window. Since A14-A16 of the
 		// processor are unconditionally replaced with these bits when
 		// B8000h is accessed, the 16kB area is mapped to the 32kB
 		// range twice in a row. (Scuba Venture writes across the boundary)
-		
+
 		// Bit 6-7: Video Address mode
 		// 0: CRTC addresses A0-12 directly, accessing 8k characters
 		//    (+8k attributes). Used in text modes (one bank).
@@ -692,8 +695,8 @@ static void CycleHercPal(bool pressed) {
 	Herc_Palette();
 	VGA_DAC_CombineColor(1,7);
 }
-	
-void Herc_Palette(void) {	
+
+void Herc_Palette(void) {
 	switch (herc_pal) {
 	case 0:	// White
 		VGA_DAC_SetEntry(0x7,0x2a,0x2a,0x2a);
@@ -713,7 +716,7 @@ void Herc_Palette(void) {
 static void write_hercules(Bitu port,Bitu val,Bitu /*iolen*/) {
 	switch (port) {
 	case 0x3b8: {
-		// the protected bits can always be cleared but only be set if the 
+		// the protected bits can always be cleared but only be set if the
 		// protection bits are set
 		if (vga.herc.mode_control&0x2) {
 			// already set
@@ -786,6 +789,7 @@ Bitu read_herc_status(Bitu /*port*/,Bitu /*iolen*/) {
 	if ((retval&0x81)==0x80) retval |= 0x8;
 	return retval;
 }
+
 
 void VGA_SetupOther(void) {
 	Bitu i;
@@ -862,4 +866,5 @@ void VGA_SetupOther(void) {
 			IO_RegisterReadHandler(base+port_ct*2+1,read_crtc_data_other,IO_MB);
 		}
 	}
+
 }
